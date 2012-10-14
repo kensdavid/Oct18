3d62f3d5b55560ba0090f4801ba4e876c211d5c3
Ken David <ken.s.david@gmail.com>
2012-10-14 10:44:57 -0400

    Initial Commit

---
+++ Oct18/Oct18AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Oct18AppDelegate.h
+//  Oct18
+//
+//  Created by Kenneth David on 10/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Oct18AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Oct18/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Oct18
+//
+//  Created by Kenneth David on 10/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Oct18AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Oct18AppDelegate class]));
+    }
+}
